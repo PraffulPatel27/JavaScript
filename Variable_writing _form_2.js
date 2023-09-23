a1204b6f323bea4a0bcd0e77f2257008d6f55fc0
PraffulPatel27 <prafful123patel@gmail.com>
2023-09-24 00:04:51 +0530

    Updating files

--- Variable_writing _form_2.js
+++ Variable_writing _form_2.js
@@ -3,3 +3,19 @@
 // *********** let keyword ****************
 // ******** declare variable with let keyword **********
 
+// let firstName= "Prafful Patel";
+// console.log(firstName);;
+
+// var demoName= "Match";
+// var demoName= "New-match";
+// console.log(demoName);
+ 
+// let demoName= "Match";
+// let demoName= "New-match";   //this line gives error because it is already declared
+// console.log(demoName);
+
+let demoName= "Match";
+demoName= "New-match";
+console.log(demoName);  //********It will run ********/
+
+//**** Learn in future ******
