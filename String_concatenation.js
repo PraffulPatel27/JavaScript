ecab387f1d1311ba0243c429bd4fe860d8f6fd3a
PraffulPatel27 <prafful123patel@gmail.com>
2023-09-25 17:35:34 +0530

    string concatenation

---
+++ String_concatenation.js
@@ -0,0 +1,22 @@
+//**** String concatenation  *****/
+
+let str1 = "Namaste";
+let str2 = "JavaScript";
+
+let fullstr = str1 + " "  + str2;  //Three string concatenate
+console.log(fullstr);
+
+//Question
+let string1 = "12";
+let string2 = "10";
+
+let ansstring = string1 + string2;
+console.log(ansstring);   //"1210"
+
+let string3 = "12";
+let string4 = "10";
+
+let anstring = +string3 + +string4;
+console.log(anstring);   //22
+console.log(typeof anstring);  //number
+
