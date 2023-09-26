93ec778005e2c7db771a83c5bede1c4fa3287679
PraffulPatel27 <prafful123patel@gmail.com>
2023-09-26 23:51:36 +0530

    if else condition

---
+++ if_else_condition.js
@@ -0,0 +1,23 @@
+//******* If else condition *******/
+
+let age = 19;
+
+if (age >= 18)
+{
+    console.log("user can play pubg game");
+}
+else {
+    console.log ("user can play mario game");
+}
+
+//******* check even or odd *******/
+
+let num = 14;
+
+if (num%2 === 0)
+{
+    console.log("number is even");
+}
+else{
+    console.log("number is odd");
+}
