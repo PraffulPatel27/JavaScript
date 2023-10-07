0f4ab5c2883d5feb181e3eaca4f72f51b3ff8261
PraffulPatel27 <prafful123patel@gmail.com>
2023-10-07 17:11:54 +0530

    variables

---
+++ Agni path/Zero_to_Datatypes.js
@@ -0,0 +1,24 @@
+//  ***** Dataypes ******* 
+"use strict";   // treat all JS code as newer version
+
+// alert (3 + 3)  // we are using nodejs, not browser
+
+let string = "Hello World";
+let age = 21;
+let isLoggedIn = false;
+
+    /****************************************/
+  //   number => 2 to power 53
+  //   bigint => 2 to power 53 and above
+  //   string => ""
+  //   boolean => true or false
+  //   null => standalone value
+  //   undefined => 
+  //   symbol => unique 
+    /****************************************/
+
+console.log(typeof string);    // string
+console.log(typeof age);       // number
+console.log(typeof null);      // object
+console.log(typeof undefined); // undefined
+console.log(typeof isLoggedIn); // boolean
