d8ab700017ad2030125b4398afb5a17c6bdf38df
PraffulPatel27 <prafful123patel@gmail.com>
2023-10-07 22:56:17 +0530

    Comparison Datatypes

---
+++ Agni path/Comparison_datatypes.js
@@ -0,0 +1,16 @@
+// ********* Comparision of datatypes *********
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   // true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0); 
+
+
+// ******* === ******* 
+
+console.log(" == ","2" == 2);  // true
+
+console.log(" === ","2" === 2);  // false
