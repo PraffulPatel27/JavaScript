1e63f8eb5d20dd54bba880ec7bba24175c58cbbe
PraffulPatel27 <prafful123patel@gmail.com>
2023-10-07 22:45:36 +0530

    JS Operation

--- Agni path/Conversion_Operation.js
+++ Agni path/Conversion_Operation.js
@@ -1,68 +1,96 @@
 // **** Conversion Operation ****
 
-let score = 33;
-console.log(typeof score); // number
-console.log(typeof (score)); // number
+// let score = 33;
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
 
-score = "33";
-console.log(typeof (score));
-let valueIntNumber = Number(score);
-console.log(typeof valueIntNumber); // number
+// score = "33";
+// console.log(typeof (score));
+// let valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber); // number
 
-score = "123abs";
-console.log(typeof (score));
-valueIntNumber = Number(score);
-console.log(typeof valueIntNumber);  // number
-console.log(valueIntNumber);   // NaN  (Not a Number)
+// score = "123abs";
+// console.log(typeof (score));
+// valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber);  // number
+// console.log(valueIntNumber);   // NaN  (Not a Number)
 
-score = null;
-console.log(typeof (score));  // object
-valueIntNumber = Number(score);
-console.log(typeof valueIntNumber);  // number
-console.log(valueIntNumber);  // 0
+// score = null;
+// console.log(typeof (score));  // object
+// valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber);  // number
+// console.log(valueIntNumber);  // 0
 
-score = undefined;
-console.log(typeof (score));  // undefined
-valueIntNumber = Number(score);
-console.log(typeof valueIntNumber);  // number
-console.log(valueIntNumber);  // NaN
+// score = undefined;
+// console.log(typeof (score));  // undefined
+// valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber);  // number
+// console.log(valueIntNumber);  // NaN
 
-score = true;
-console.log(typeof (score));   // boolean
-valueIntNumber = Number(score);
-console.log(typeof valueIntNumber);  // number
-console.log(valueIntNumber); // 1
+// score = true;
+// console.log(typeof (score));   // boolean
+// valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber);  // number
+// console.log(valueIntNumber); // 1
 
-score = "prafful";
-console.log(typeof (score));   // string
-valueIntNumber = Number(score);
-console.log(typeof valueIntNumber);  // number
-console.log(valueIntNumber);   // NaN
+// score = "prafful";
+// console.log(typeof (score));   // string
+// valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber);  // number
+// console.log(valueIntNumber);   // NaN
 
-// "33" => 33
-// "123abs" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "123abs" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = 1;
-let bool = Boolean(isLoggedIn);
-console.log(typeof bool);  // boolean
-console.log(bool); // true
+// let isLoggedIn = 1;
+// let bool = Boolean(isLoggedIn);
+// console.log(typeof bool);  // boolean
+// console.log(bool); // true
 
-isLoggedIn = "";
-bool = Boolean(isLoggedIn);
-console.log(typeof bool);  // boolean
-console.log(bool);  // false
+// isLoggedIn = "";
+// bool = Boolean(isLoggedIn);
+// console.log(typeof bool);  // boolean
+// console.log(bool);  // false
  
-isLoggedIn = "prafful";
-bool = Boolean(isLoggedIn);
-console.log(typeof bool);  // boolean
-console.log(bool);  // true
+// isLoggedIn = "prafful";
+// bool = Boolean(isLoggedIn);
+// console.log(typeof bool);  // boolean
+// console.log(bool);  // true
 
-// 1 => true; 0 => false
-// "" => false; 
-// "prafful" => true
+// // 1 => true; 0 => false
+// // "" => false; 
+// // "prafful" => true
 
-let num = 10;
-let string = String(num);
-console.log(typeof string);  
-console.log(string);  
+// let num = 10;
+// let string = String(num);
+// console.log(typeof string);  
+// console.log(string);  
+
+
+// *************** Operations ************ 
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+console.log("1"  + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);    // first string then behave as string
+console.log(1 + 2 + "2");    // first number then behave as number
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
+
+// https://tc39.es/ecma262/multipage/abstract—operations.html#sec—type-conversion
