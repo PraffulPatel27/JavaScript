7a3e591a177bc280024cfdfc72f7bc8404191885
PraffulPatel27 <prafful123patel@gmail.com>
2023-10-11 15:18:48 +0530

    Nums and Math Function

--- Agni path/Nums_math.js
+++ Agni path/Nums_math.js
@@ -1,42 +1,42 @@
 // ******** Nums and Math ********
 
-// const score = 400;
-// console.log(score);
+const score = 400;
+console.log(score);
 
-// const balance = new Number(100)
-// console.log(balance);
-// console.log(typeof balance);
+const balance = new Number(100)
+console.log(balance);
+console.log(typeof balance);
 
-// console.log(balance.toString());    //convert int to string
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(3));
+console.log(balance.toString());    //convert int to string
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
 
 
-// //Precision function 
-// const NewNumber = 23.8966        // take 3 digit after decimal
-// console.log(NewNumber.toPrecision(3));
+//Precision function 
+const NewNumber = 23.8966        // take 3 digit after decimal
+console.log(NewNumber.toPrecision(3));
 
-// const OtherNumber = 123.8966;
-// console.log(OtherNumber.toPrecision(3));
+const OtherNumber = 123.8966;
+console.log(OtherNumber.toPrecision(3));
 
-// const advnum = 1223.8966;
-// console.log(advnum.toPrecision(3));
+const advnum = 1223.8966;
+console.log(advnum.toPrecision(3));
 
-// const nums = 1000000
-// console.log(nums.toLocaleString());      // US standard
-// console.log(nums.toLocaleString('en-IN'));  //Indian standard 
+const nums = 1000000
+console.log(nums.toLocaleString());      // US standard
+console.log(nums.toLocaleString('en-IN'));  //Indian standard 
 
-// console.log(Number.MAX_VALUE);   // max value of number
-// console.log(Number.MIN_VALUE);   // min value of number
+console.log(Number.MAX_VALUE);   // max value of number
+console.log(Number.MIN_VALUE);   // min value of number
 
-// console.log(Number.MAX_SAFE_INTEGER);   // max safe value of number
-// console.log(Number.MIN_SAFE_INTEGER);   // min safe value of number
+console.log(Number.MAX_SAFE_INTEGER);   // max safe value of number
+console.log(Number.MIN_SAFE_INTEGER);   // min safe value of number
 
-// const n = 200
-// console.log(Number.isInteger(n));   // check number is integer or not
-//                                     // return true or false
-// const str = "Integer" 
-// console.log(Number.isInteger(str));
+const n = 200
+console.log(Number.isInteger(n));   // check number is integer or not
+                                    // return true or false
+const str = "Integer" 
+console.log(Number.isInteger(str));
 
 
 // ++++++++++++++++++ Maths +++++++++++++++++++++
